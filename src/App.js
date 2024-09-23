8e42119db721cef8569e7c230789ff7915d2a634
KinngShishupal <shishupalsingh1947@gmail.com>
2024-09-23 23:39:23 +0530

    pushinf complete code

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import Tile from './Tile';
+import './tiles.css'
+const arr = [...Array(9)];
+const App = () => {
+  const [selectedTiles, setSelectedTiles] = useState([]);
+
+  useEffect(() => {
+    let intervalId=null;
+    if (selectedTiles.length === 8) {
+       intervalId = setInterval(() => {
+        setSelectedTiles(p => {
+          const t = [...p];
+          t.pop();
+          if (t.length === 0 ) {
+            clearInterval(intervalId);
+          }
+          return t;
+        })
+      }, 300)
+
+    }
+
+  }, [selectedTiles])
+
+
+
+  const onClickHanlder = (index) => {
+    setSelectedTiles(t => [...t, index]);
+  };
+
+  return (
+    <>
+      <div className='tiles__container'>
+        {
+          arr.map((_, index) => {
+            return <Tile className={`tile_${index} ${selectedTiles.includes(index) ? 'green' : ''}`} onClick={() => onClickHanlder(index)} />
+          })
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
